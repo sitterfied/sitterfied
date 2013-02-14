4d51f5ed2fd8b63c4ab30fbb909193bdff99f9f7
Ben Beecher <benbeecher@gmail.com>
2013-02-14 01:11:31 -0500

    using useradmin

--- sitterfied/app/admin.py
+++ sitterfied/app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 
+
 admin.site.register(ComingSoonInterest)
+
+admin.site.register(User, UserAdmin)
