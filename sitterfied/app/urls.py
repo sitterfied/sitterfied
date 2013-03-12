5c44ed93c1277592b62d57b8d7912a2291f69af9
Ben Beecher <benbeecher@gmail.com>
2013-03-12 14:03:27 -0400

    hiding the video

--- sitterfied/app/urls.py
+++ sitterfied/app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
-from django.views.generic.simple import direct_to_template
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('app.views',
@@ -8,7 +8,7 @@ urlpatterns = patterns('app.views',
     url(r'^email_submit/$', 'comingsoon_email_submit', name='comingsoon_email_submit'),
     url(r'^invite_email_submit/$', 'invite_email_submit', name='invite_email_submit'),
     url(r'^$', 'index'),
-    url(r'^about/$', direct_to_template, {'template': 'howitworks.html'}, name="howitworks"),
+    url(r'^about/$', TemplateView.as_view(template_name='howitworks.html'), name="howitworks"),
     url(r'^refer/(?P<referred_by>\d+)$', 'index'),
 
 
