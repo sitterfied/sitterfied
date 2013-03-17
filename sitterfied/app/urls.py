5dff185cab14eb53b07f1c25fd5a3bbd1c256b93
Ben Beecher <benbeecher@gmail.com>
2013-03-16 23:25:36 -0400

    bunch of tweaks

--- sitterfied/app/urls.py
+++ sitterfied/app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('app.views',
     # Examples:
     url(r'^email_submit/$', 'comingsoon_email_submit', name='comingsoon_email_submit'),
     url(r'^invite_email_submit/$', 'invite_email_submit', name='invite_email_submit'),
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name="home"),
     url(r'^referrals/$', 'referral_tracking', name="referral_tracking"),
     url(r'^about/$', TemplateView.as_view(template_name='howitworks.html'), name="howitworks"),
     url(r'^refer/(?P<referred_by>\d+)$', 'index'),
