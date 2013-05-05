3823a469161012680f31913f1fe44676ca73382d
Ben Beecher <benbeecher@gmail.com>
2013-05-04 22:42:54 -0400

    fixing admin form for users

--- sitterfied/app/admin.py
+++ sitterfied/app/admin.py
@@ -1,9 +1,38 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import *
 
 
 
 admin.site.register(ComingSoonInterest)
 
-admin.site.register(User, UserAdmin)
+
+
+
+class SitterUserCreationForm(UserCreationForm):
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            # Not sure why UserCreationForm doesn't do this in the first place,
+            # or at least test to see if _meta.model is there and if not use User...
+            self._meta.model._default_manager.get(username=username)
+        except self._meta.model.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    class Meta:
+        model = User
+        fields = ("username",)
+
+class SitterUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+
+class SitterUserAdmin(UserAdmin):
+    form = SitterUserChangeForm
+    add_form = SitterUserCreationForm
+
+
+admin.site.register(User, SitterUserAdmin)
