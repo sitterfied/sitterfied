2243bda0f6d9f996a4129bf0cddf06e7d625164a
Ben Beecher <benbeecher@gmail.com>
2013-07-11 15:43:32 -0400

    using data folder for avatars

--- sitterfied/sitterfied/settings/production.py
+++ sitterfied/sitterfied/settings/production.py
@@ -28,7 +28,7 @@ DATABASES = {
         'USER': env.get('DOTCLOUD_DATA_SQL_LOGIN', ''),
         'PASSWORD': env.get('DOTCLOUD_DATA_SQL_PASSWORD', ''),
         'HOST': env.get('DOTCLOUD_DATA_SQL_HOST', ''),
-        'PORT': int(env.get('DOTCLOUD_DATA_SQL_PORT', '0')),
+        'PORT': int(env.get('DOTCLOUD_DATA_SQL_PORT', '')),
     }
 }
 
@@ -36,7 +36,7 @@ DATABASES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = "/home/dotcloud/data/media/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
