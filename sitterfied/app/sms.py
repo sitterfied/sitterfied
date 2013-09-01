178c386807391d46665eddb0d93594d6e8aac740
Ben Beecher <benbeecher@gmail.com>
2013-09-01 01:07:24 -0400

    finished up sms accepting

--- sitterfied/app/sms.py
+++ sitterfied/app/sms.py
@@ -5,61 +5,59 @@ from django_twilio.decorators import twilio_view
 
 
 from twilio.rest import TwilioRestClient
+from twilio.twiml import Response
+
+from .models import Sitter, Booking
+
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-
-
 account_sid = settings.TWILIO_ACCOUNT_SID
 auth_token  = settings.TWILIO_AUTH_TOKEN
 sitterfied_number = settings.TWILIO_DEFAULT_CALLERID
 
 
 client = TwilioRestClient(account_sid, auth_token)
+from django.http import HttpResponse
 
-# def send_booking_request(sender, instance, created, **kwargs):
-#     if not created:
-#         return
-#     body = "Babysit from " + instance.booking.start_date_time + "to " + instance.booking.stop_date_time + "? Please text '" + instance.id +":yes' to accept "
 
-#     to = instance.sitter.cell
-#     client.sms.messages.create(body=body,
-#                                to=to,
-#                                from_=sitterfied_number)
 
-# @twilio_view
-# def sms_messages(request):
-#     from_number = request.values.get('From', None)
-#     try:
-#         sitter = Sitter.objects.get(cell=from_number)
-#     except Sitter.DoesNotExist:
-#         return
-#     body = request.values.get('body')
-#     yes = re.search("(\d*):yes", body)
-#     request_id = yes.groups()[0]
-#     try:
-#         booking_request = BookingRequest.objects.get(id=request_id)
-#         booking_request.sitter_accepted = True
-#         booking_request.save()
-#     except BookingRequest.DoesNotExist:
-#         return
-
-# def get_new_messages():
-#     try:
-#         most_recent_message = IncomingSMSMessage.objects.all().order_by('-date_created')[0]
-#         most_recent  = most_recent_message.date_created
-#         messages = client.sms.messages.list(to=sitterfied_number, after=most_recent)
-#     except IndexError:
-#         print "no messages yet"
-#         messages = client.sms.messages.list(to=sitterfied_number)
-#     for message in messages:
-#         new_message = IncomingSMSMessage(**message)
-#         new_message.save()
+@twilio_view
+def sms_messages(request):
+    from_number = request.POST.get('From', None)
+    resp = Response()
+    if from_number.startswith("+1"):
+        from_number = from_number[2:]
+    try:
+        sitter = Sitter.objects.get(cell=from_number)
+    except Sitter.DoesNotExist:
+        resp.sms("Sorry, but this cell phone isn't registered")
+        return str(resp)
+    body = request.POST.get('Body')
+    yes = re.search("(\d*):yes", body)
+    request_id = yes.groups()[0]
+    try:
+        booking = Booking.objects.get(id=request_id)
+        if booking.accepted_sitter:
+            resp.sms("Sorry, but this booking has already been accepted")
+            return str(resp)
+        booking.accepted_sitter = sitter
+        booking.save()
+    except Booking.DoesNotExist:
+        resp.sms("Sorry, but you've tried to accept a booking that doesnt' exist")
+        return str(resp)
 
 
 
 
-
-
-# from django.db.models.signals import post_save
-# from .models import BookingRequest
-# post_save.connect(send_booking_request, sender=BookingRequest)
+#for polling via cronjob
+def get_new_messages():
+    try:
+        most_recent_message = IncomingSMSMessage.objects.all().order_by('-date_created')[0]
+        most_recent  = most_recent_message.date_created
+        messages = client.sms.messages.list(to=sitterfied_number, after=most_recent)
+    except IndexError:
+        print "no messages yet"
+        messages = client.sms.messages.list(to=sitterfied_number)
+    for message in messages:
+        new_message = IncomingSMSMessage(**message)
+        new_message.save()
--- sitterfied/app/urls.py
+++ sitterfied/app/urls.py
@@ -46,6 +46,7 @@ urlpatterns += patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', name="login", ),
     url(r'^password_change/$', 'django.contrib.auth.views.password_change', {'post_change_redirect':'/'}, name="password_change", ),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}, name="logout", ),
+    url(r'^sms/$', 'app.sms.sms_messages',  name="sms_messages"),
     #Finally our catch all
     url(r'^.*/$', 'app.views.index', name='index'),
     url(r'^$', 'app.views.index', name='index'),
