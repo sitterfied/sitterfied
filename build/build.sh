fd61d9cd489e3511d81ed350bfe9492a9063f2e8
Ben Beecher <benbeecher@gmail.com>
2013-09-11 16:01:21 -0400

    rebuilt sitterfied.js, using node for build rather than rhino

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,6 @@ pushd .
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR=$DIR/../sitterfied/static_source/js/
 cd $OUTPUT_DIR
-java -classpath $DIR/js.jar:$DIR/compiler.jar org.mozilla.javascript.tools.shell.Main $DIR/r.js -o app.build.js
+node $DIR/r.js -o app.build.js
 java -jar $DIR/compiler.jar --js sitterfied.js --language_in ECMASCRIPT5 --js_output_file sitterfied-min.js
 popd
