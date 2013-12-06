89dc7c2aa63c3c12c047392bbf296a0df3bd95e0
Andrew Lewisohn <andrewl@arc90.com>
2013-12-06 15:16:28 -0500

    Documentation and build process modifications.

--- templates.py
+++ templates.py
@@ -1,5 +1,7 @@
 import os
 import time
+import sys
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -8,10 +10,14 @@ modules = ['sitterfied']
 thisdir = os.path.dirname(os.path.realpath(__file__))
 module_dirs =  [os.path.join(thisdir, mod, "static_source", "js", "templates") for mod in modules]
 
-
 import fnmatch
 import os
 
+def safe_list_get (l, idx, default):
+  try:
+    return l[idx]
+  except IndexError:
+    return default
 
 def compile_templates():
     print "compiling templates..."
@@ -47,18 +53,19 @@ class Compiler(FileSystemEventHandler):
 
 if __name__ == "__main__":
     compile_templates()
-    observer = Observer()
-    compiler = Compiler()
-    print module_dirs
-    for module_dir in module_dirs:
-        for root, dirnames, filenames in os.walk(module_dir):
-            for dirname in dirnames:
-                dir = os.path.join(root, dirname)
-                observer.schedule(compiler, dir)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    if safe_list_get(sys.argv, 1, 'watch') is 'watch':
+        observer = Observer()
+        compiler = Compiler()
+        print module_dirs
+        for module_dir in module_dirs:
+            for root, dirnames, filenames in os.walk(module_dir):
+                for dirname in dirnames:
+                    dir = os.path.join(root, dirname)
+                    observer.schedule(compiler, dir)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
