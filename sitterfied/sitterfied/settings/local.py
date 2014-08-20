062d1d4e0fa584f4f3ec35736778708bccb43724
Andrew Lewisohn <andrewl@maximus.local>
2014-08-20 12:23:25 -0400

    Modifications for running the site on a vagrant box under uwsgi and nginx.

--- sitterfied/sitterfied/settings/local.py
+++ sitterfied/sitterfied/settings/local.py
@@ -6,7 +6,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 INTERNAL_IPS = (
     '127.0.0.1',
-    '192.168.111.1',
+    '192.168.100.22',
 )
 
 
@@ -33,38 +33,6 @@ MEDIA_URL = "/media/"
 GOOGLE_OAUTH_CLIENT_ID = '213296649304-10d5ppglkmjmq7l60uub4r5nvnklfjoa.apps.googleusercontent.com'
 GOOGLE_OAUTH_CLIENT_SECRET = 'u9BePfC9OC9Z3XkO0SMJbwkq'
 GOOGLE_OAUTH_REDIRECT_URI = "http://sitterfied.ngrok.com"
-
-INSTALLED_APPS += (
-    #'debug_toolbar',
-    'devserver',
-)
-
-MIDDLEWARE_CLASSES += (
-    #'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
-
-
-DEVSERVER_ARGS = ['--werkzeug']
-
-
-DEVSERVER_MODULES = (
-    'devserver.modules.sql.SQLRealTimeModule',
-    'devserver.modules.sql.SQLSummaryModule',
-    'devserver.modules.profile.ProfileSummaryModule',
-
-    # # Modules not enabled by default
-    'devserver.modules.ajax.AjaxDumpModule',
-    'devserver.modules.profile.MemoryUseModule',
-    'devserver.modules.cache.CacheSummaryModule',
-    'devserver.modules.profile.LineProfilerModule',
-)
-
-
-DEVSERVER_AUTO_PROFILE = True
 
 
 # Override Short URL
