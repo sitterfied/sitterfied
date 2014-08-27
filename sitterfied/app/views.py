777a337aabc4b26791b2da7aa54373482e5a9684
Andrew Lewisohn <andrewl@maximus.local>
2014-08-27 11:20:04 -0400

    Adds support for Sidewswipe.io.

--- sitterfied/app/views.py
+++ sitterfied/app/views.py
@@ -35,6 +35,8 @@ from app.utils import send_html_email
 # Universal params used in all views
 view_params = {
     'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID,
+    'POPCORN_METRICS_ID': settings.POPCORN_METRICS_ID,
+    'SIDESWIPE_IO_TOKEN': settings.SIDESWIPE_IO_TOKEN,
     'UPLOADCARE_PUBLIC_KEY': settings.UPLOADCARE['pub_key'],
 }
 
