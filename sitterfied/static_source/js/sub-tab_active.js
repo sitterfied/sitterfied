63aff57e66cf6e4f06272d57fa752fff9f40d7b8
Jamie Newell <hello@jamienewell.me>
2015-01-08 09:54:39 -0500

    Highlight active sub-navigation items

--- sitterfied/static_source/js/sub-tab_active.js
+++ sitterfied/static_source/js/sub-tab_active.js
@@ -1,19 +1,20 @@
-jQuery(function() {
-			if(jQuery('.tab_content .subtab_select li:nth-child(2)').hasClass('active')) { 
+/*jQuery(function() {
+
+			if(jQuery('.tab_content .subtab_select li:nth-child(2) a').hasClass('active')) { 
 					jQuery('.tab_content .subtab_select').removeClass('movebg2 movebg3 movebg4 movebg5 movebg6').addClass('movebg');
-		        } else if(jQuery('.tab_content .subtab_select li:nth-child(3)').hasClass('active')) {
+		        } else if(jQuery('.tab_content .subtab_select li:nth-child(3) a').hasClass('active')) { console.log('woohoo2');
 					jQuery('.tab_content .subtab_select').removeClass('movebg3 movebg4 movebg5 movebg6').addClass('movebg movebg2');
-		        } else if(jQuery('.tab_content .subtab_select li:nth-child(4)').hasClass('active')) {
+		        } else if(jQuery('.tab_content .subtab_select li:nth-child(4) a').hasClass('active')) {
 					jQuery('.tab_content .subtab_select').removeClass('movebg2 movebg4 movebg5 movebg6').addClass('movebg movebg3');
-		        } else if(jQuery('.tab_content .subtab_select li:nth-child(5)').hasClass('active')) {
+		        } else if(jQuery('.tab_content .subtab_select li:nth-child(5) a').hasClass('active')) {
 					jQuery('.tab_content .subtab_select').removeClass('movebg3 movebg2 movebg5 movebg6').addClass('movebg movebg4');
-		        } else if(jQuery('.tab_content .subtab_select li:nth-child(6)').hasClass('active')) {
+		        } else if(jQuery('.tab_content .subtab_select li:nth-child(6) a').hasClass('active')) {
 					jQuery('.tab_content .subtab_select').removeClass('movebg3 movebg4 movebg2 movebg6').addClass('movebg movebg5');
-		        } else if(jQuery('.tab_content .subtab_select li:nth-child(7)').hasClass('active')) {
+		        } else if(jQuery('.tab_content .subtab_select li:nth-child(7) a').hasClass('active')) {
 					jQuery('.tab_content .subtab_select').removeClass('movebg3 movebg4 movebg5 movebg2').addClass('movebg movebg6');
 		        } else {
 					jQuery('.tab_content .subtab_select').removeClass('movebg movebg2 movebg3 movebg4 movebg5 movebg6');
 		        }; 
 				
 				
-});
+});*/
