82c20dc2d34b999bf60beabd72a659ad998ec07a
Andrew Lewisohn <andrewl@maximus.local>
2015-03-05 12:33:36 -0500

    Cleaning up.
    [skip ci]

--- aws/user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LINE=$(grep -n "DJANGO_SETTINGS_MODULE" /etc/environment | cut -f 1 -d :)
-sed -i $LINE"s/.*/DJANGO_SETTINGS_MODULE=sitterfied.settings.production/" /etc/environment
-
-service uwsgi restart
-service beatcop restart
-service celeryd restart
