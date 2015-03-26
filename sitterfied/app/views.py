521caff84fb0d3dd3708289381468529d62f4a6f
Andrew Lewisohn <andrewl@maximus.local>
2015-03-26 18:10:03 -0400

    Consistent naming.

--- sitterfied/app/views.py
+++ sitterfied/app/views.py
@@ -42,7 +42,7 @@ from sitterfied.utils.models import Group
 # Universal params used in all views
 view_params = {
     'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID,
-    'SEGMENT_API_TOKEN': settings.SEGMENT_API_KEY,
+    'SEGMENT_API_KEY': settings.SEGMENT_API_KEY,
     'SIDESWIPE_IO_TOKEN': settings.SIDESWIPE_IO_TOKEN,
     'UPLOADCARE_PUBLIC_KEY': settings.UPLOADCARE['pub_key'],
     'UPLOADCARE_CDN_BASE': settings.UPLOADCARE['upload_base_url'],
